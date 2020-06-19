6ebef8b7289f438e3bb40f1e774934312b9af73a
Alex <afriedman1991@gmail.com>
2020-06-19 16:28:37 -0400

    Attached camera to player. Camera follows player

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -6,9 +6,14 @@ public class Cam : MonoBehaviour
 {
     public Transform player;
     float heading = 0;
+    float tilt = 15;
+    float camDist = 10;
 
     void Update()
     {
         heading += Input.GetAxis("Mouse X") * Time.deltaTime * 180;
+        transform.rotation = Quaternion.Euler(tilt, heading, 0);
+
+        transform.position = player.position - transform.forward * camDist;
     }
 }
